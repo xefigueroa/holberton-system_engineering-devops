944fb82e5cd9f8cae7b7932f50ce6168547f8f8b
xefigueroa <xefigueroa@github.com>
2021-08-30 03:29:30 +0000

    adv

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#Creates Sets up client SSH configuration file
+file_line { 'ssh_config':
+    ensure   => 'present',
+    path     => '/etc/ssh/ssh_config',
+    line     => 'PasswordAuthentication no',
+}
+file_line { 'ssh_config_2':
+    ensure   => 'present',
+    path     => '/etc/ssh/ssh_config',
+    line     => 'IdentityFile ~/.ssh/holberton',
+}

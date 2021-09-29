a9afd561c996f7021feabdbfa9d7332dcd0692ca
xefigueroa <xefigueroa@github.com>
2021-09-29 01:59:03 +0000

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,17 +6,18 @@ import requests
 
 def top_ten(subreddit):
     """prints titles of 10 1st hot posts"""
-    if subreddit is None or type(subreddit) is not str:
-        return print("None")
-
     BASE_URL = 'http://www.reddit.com/r/{}/about.json'
     head = {'User-Agent': 'Mozilla/5.0'}
     par = {'limit': 10}
 
     r = requests.get(BASE_URL.format(subreddit), headers=head,
                      params=par, allow_redirect=False)
+
+    if r.status_code == 404:
+        print("None")
+        return
+
     data = r.json().get('data', {}).get('children', {})
-    if r.status_code != 200 or not data:
-        return print("None")
+
     for post in data:
         print(data.get('data', {}).get('title'))

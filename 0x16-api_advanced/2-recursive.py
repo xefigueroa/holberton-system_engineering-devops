dffcf06026e110b225d9e2a138edcea60e47247a
xefigueroa <xefigueroa@github.com>
2021-09-29 02:35:59 +0000

    0x16

---
+++ 0x16-api_advanced/2-recursive.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""queries Reddit API, returns articles w/titles for subreddit."""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """prints titles of 10 1st hot posts"""
+    BASE_URL = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    head = {'User-Agent': 'Mozilla/5.0'}
+    par = {'after': after}
+
+    r = requests.get(BASE_URL, params=par, headers=head).json()
+
+    res = r.get('data', {}).get('children', None)
+    control = r.get('data', {}).get('after', None)
+
+    if control:
+        if res:
+            for hot in res:
+                hot_list.append(item.get('data').get('title'))
+        if control:
+            recurse(subreddit, hot_list, control)
+        return hot_list
+    else:
+        return None
